9c2fd8d9835e6cabc64a8cc6b198c8f10c08eff5
farnazzam <far.zamiri75@gmail.com>
2022-05-16 12:49:26 -0400

    google calendar

---
+++ Unity-Google-Calendar-main/Assets/Scripts/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleCalendarUnity;
+
+public class Testing : MonoBehaviour
+{
+
+
+void Start()
+{
+    Calendar calendar = new Calendar();
+    var test = calendar.Main("3256");
+    //foreach(var ev in test)
+        Debug.Log(test);
+}
+
+
+
+
+
+}
